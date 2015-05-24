8c4d745be0d9eda362215b1b31de8b9c53bf55dd
geansru <d@geans.ru>
2015-05-24 16:42:08 +0700

    modified:   functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check
         # out its homepage
@@ -36,18 +42,15 @@ class NewVisitorTest(unittest.TestCase):
         # tying fly-fishing lures)
         input_box.send_keys('Buy peacock feathers')
 
+        asserts = [
+            '%d: Buy peacock feathers',
+            '%d: Use peacock feathers to make a fly',
+        ]
+
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         input_box.send_keys(Keys.ENTER)
-
-        time.sleep(3)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            msg="New to-do item did not appear in the table -- its text was %s" % (table.text)
-        )
+        self.check_for_row_in_list_table(asserts[0] % 1)
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -56,23 +59,12 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
 
         # The page updates again, and shows both items on her list
-        table = self.browser.find_elements_by_id('id_list_table')
-        rows = self.browser.find_elements_by_tag_name('tr')
-        ary = [row.text for row in rows]
-        asserts = [
-            '%d: Buy peacock feathers',
-            '%d: Use peacock feathers to make a fly',
-        ]
         for i in range(0, len(asserts)):
             number = i + 1
             assert_string = asserts[i] % number
-            # print(assert_string)
-            self.assertIn(assert_string , ary)
-        # self.assertIn(, ary)
+            self.check_for_row_in_list_table(assert_string)
 
-
-
-
+        # Edith wonders whether the site will remember her list. Then she sees
         # [...]
         self.fail('Finish the test!')
 
