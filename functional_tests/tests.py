bd07aa8633acce09f8253f9752fa0a379a37d0a9
geansru <d@geans.ru>
2015-05-26 09:35:46 +0700

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
+import sys
 
 __author__ = 'noskill'
 from selenium import webdriver
@@ -22,10 +23,11 @@ class NewVisitorTest(StaticLiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
-    def _test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes to check
         # out its homepage
-        self.browser.get(self.live_server_url)
+        # self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn(self.title, self.browser.title)
@@ -74,7 +76,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page. There is no sign of Edith's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -86,7 +88,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         input_box.send_keys(Keys.ENTER)
 
         # Francis gets his own unique URL
-        francis_list_url= self.browser.current_url
+        francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEquals(francis_list_url, edith_list_url)
 
@@ -129,3 +131,17 @@ class NewVisitorTest(StaticLiveServerTestCase):
         input_box_location = get_location()
         # self.assertAlmostEqual(input_box_location, x, delta=delta)
         check_center()
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
